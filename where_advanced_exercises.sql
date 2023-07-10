f1e3f547ea5e2f06faa2426ea015b92e325f49e8
Aswathy Radha <aswathy.radha@gmail.com>
2023-07-10 00:34:03 -0500

    Updated where_advanced_exercises

--- where_advanced_exercises.sql
+++ where_advanced_exercises.sql
@@ -1,6 +1,7 @@
 SHOW DATABASES;
 USE employees;
 DESCRIBE employees;
+-- # 1
 -- Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya'
 --  using IN. What is the employee number of the top three results?
 
@@ -15,6 +16,7 @@ LIMIT 3 ;
 -- ANSWER: 10200
 -- 10397
 -- 10610
+-- # 2
 -- Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya',
 -- as in Q2, but use OR instead of IN. 
 -- What is the employee number of the top three results? Does it match the previous question?
@@ -32,7 +34,7 @@ last_name
 -- 10610
 -- Yes it matches the previous
 DESCRIBE employees;
-
+-- # 3
  SELECT emp_no,
  first_name,
  last_name,
@@ -44,6 +46,24 @@ DESCRIBE employees;
 -- ANSWER: 10200
 -- 10397
 -- 10821
+
+-- #4  Find all unique last names that start with 'E'.
+
+SELECT DISTINCT
+ last_name
+ FROM employees
+ WHERE last_name LIKE 'E%';
+ 
+ 
+
+-- #5  Find all unique last names that start or end with 'E'.
+
+SELECT DISTINCT
+ last_name
+ FROM employees
+ WHERE last_name LIKE 'E%'
+ OR last_name LIKE '%E';
+ 
 -- Find all unique last names that start with 'E'.
 SELECT DISTINCT
  first_name,
@@ -51,6 +71,12 @@ SELECT DISTINCT
  FROM employees
  WHERE last_name LIKE 'E%';
  
+ -- #6  Find all unique last names that end with 'E',but does not start with e.
+SELECT DISTINCT
+ last_name
+ FROM employees
+ WHERE last_name LIKE '%e'
+ AND last_name NOT LIKE 'E%';
  
  SELECT DISTINCT
  first_name,
@@ -67,7 +93,7 @@ SELECT DISTINCT
  WHERE last_name LIKE 'E%'
  AND last_name LIKE 'E%e';
  
- 
+ -- # 7
  SELECT DISTINCT
  first_name,
  last_name
@@ -100,7 +126,8 @@ last_name
  FROM employees
  WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name ='Maya'
  LIMIT 3 ;
-
+ 
+-- # 8
  SELECT emp_no
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
@@ -118,7 +145,7 @@ WHERE
 (MONTH(birth_date) = 12 AND DAYOFMONTH(birth_date) = 25)
 LIMIT 3;
 
-
+-- #9
 
 SELECT emp_no,
 first_name,
@@ -197,7 +224,7 @@ LIMIT 3;
 -- '10681','Florina','Garnier','1964-12-25'
 
 
-
+-- #10
 
 SELECT emp_no, first_name, last_name, birth_date
 FROM employees
@@ -207,9 +234,23 @@ WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
 LIMIT 3;
 DESCRIBE employees;
 
+/*
 SELECT DISTINCT
  emp_no,
  first_name,
+last_name
+FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
+*/
+
+-- # 11
+SELECT DISTINCT
+last_name
+FROM employees
+WHERE last_name LIKE '%q%';
+
+-- # 12
+SELECT DISTINCT
 last_name
 FROM employees
 WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
